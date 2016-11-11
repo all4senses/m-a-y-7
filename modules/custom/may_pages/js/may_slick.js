59bc1969de1fa42a6a6e3c86f77dc1a37347a32e
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-11-11 17:03:03 +0700

    Init

---
+++ modules/custom/may_pages/js/may_slick.js
@@ -0,0 +1,51 @@
+(function ($) {
+
+  Drupal.behaviors.may_home_parallax = {
+    attach: function (context, settings) {
+       
+      console.log('may_slick.....');
+      
+      
+      $(".slides").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true,
+        adaptiveHeight: true,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                vertical: true,
+                verticalSwiping: true,
+                adaptiveHeight: false,
+              }
+            }
+          ]
+      });
+      
+      //$('.slides').slickLightbox({
+      $('article').slickLightbox({
+        src: 'data-original', // 'src',
+        itemSelector: 'img[data-original]'
+      });
+      
+      $(".slides-arrow.first").click(function(){
+          $(this).parent().find('.slick-prev').click();
+      });
+      $(".slides-arrow.second").click(function(){
+          $(this).parent().find('.slick-next').click();
+      });
+       
+//      $('.flexslider').flexslider({
+//        animation: "slide",
+//        directionNav: false
+//      });
+       
+    }
+  };
+
+}(jQuery));
