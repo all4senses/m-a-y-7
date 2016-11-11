2992ccf76dc12bcd15ab98d6bcd4f5d0f2159ae3
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-11-11 17:57:33 +0700

    Init

--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -1,42 +1,35 @@
 <?php
+
 /**
  * @file
  * The primary PHP file for this theme.
  */
 
-
 /**
  * Implements hook_preprocess_page().
  */
 function bootstrap_may_preprocess_page(&$variables) {
-  //dpm($variables);
-  
+
   $variables['navbar_classes_array'][] = 'cd-auto-hide-header';
-  //dpm($variables['container_class'], '$variables[container_class]');
-  
   $variables['container_class'] .= ' cd-main-content sub-nav-hero';
-  
+
   if ($variables['is_front']) {
-   
-    //drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
-    
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.requestAnimationFrame.min.js');
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.easing.1.3.plus_one_argument.js');
-    
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery.parallax-scroll---gsgd/js/jquery.parallax-scroll---a4s.js');
-    
-    
-    $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
-    drupal_add_js($path_to_custom_js . 'may_home_parallax.js');
-    
-    
-    //drupal_add_library('contextual','contextual-links');
-    
+
+    /*
+      //drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
+
+      drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.requestAnimationFrame.min.js');
+      drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
+      drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.easing.1.3.plus_one_argument.js');
+
+      drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery.parallax-scroll---gsgd/js/jquery.parallax-scroll---a4s.js');
+
+
+      $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
+      drupal_add_js($path_to_custom_js . 'may_home_parallax.js');
+     */
   }
-  
-  
-  
+
   drupal_add_js('sites/all/libraries/jquery.plugins/_sliders/slick/slick.js');
   drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick/slick.css', array('weight' => 99, 'group' => 200));
   drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick/slick-theme.css', array('weight' => 99, 'group' => 200));
@@ -48,70 +41,50 @@ function bootstrap_may_preprocess_page(&$variables) {
   drupal_add_js($path_to_custom_js . 'may_slick.js');
 }
 
-
-
-
-
 function bootstrap_may_preprocess_node(&$variables) {
 
-    //dpm('in bootstrap_may_preprocess_node');
-    //dpm(array_keys($variables), '$variables keys in bootstrap_may_preprocess_node');
+  //dpm('in bootstrap_may_preprocess_node');
+  //dpm(array_keys($variables), '$variables keys in bootstrap_may_preprocess_node');
 
   /*
     static $custom_pages_css_added;
     if ($variables['type'] == 'project' && !$custom_pages_css_added) {
-        //dpm($variables, '$variables in bootstrap_may_preprocess_node');
-        //dpr($variables['theme_hook_suggestions']);
-        $custom_pages_css_added = TRUE;
-        $project_custom_css = path_to_theme() . '/css/projects.css';
-        if (file_exists($project_custom_css)) {
-            //dpm('added ' . $project_custom_css);
-            drupal_add_css($project_custom_css, array('weight' => 99, 'group' => 200));
-        }
-        
-        drupal_add_js('sites/all/libraries/jquery.plugins/_sliders/slick/slick.js');
-        drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick/slick.css', array('weight' => 99, 'group' => 200));
-        drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick/slick-theme.css', array('weight' => 99, 'group' => 200));
-        
-        drupal_add_js('sites/all/libraries/jquery.plugins/_sliders/slick-lightbox/dist/slick-lightbox.min.js');
-        drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick-lightbox/dist/slick-lightbox.css', array('weight' => 99, 'group' => 200));
-        
-        $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
-        drupal_add_js($path_to_custom_js . 'may_slick.js');
+    //dpm($variables, '$variables in bootstrap_may_preprocess_node');
+    //dpr($variables['theme_hook_suggestions']);
+    $custom_pages_css_added = TRUE;
+    $project_custom_css = path_to_theme() . '/css/projects.css';
+    if (file_exists($project_custom_css)) {
+    //dpm('added ' . $project_custom_css);
+    drupal_add_css($project_custom_css, array('weight' => 99, 'group' => 200));
     }
-    */
-    
-    //$node_parallax_js = drupal_get_path('module', 'may_pages') . '/js/parallax/pn' . $variables['nid']. '.js';
-    //dpm('js is ' . $node_parallax_js);
-//    if (file_exists($node_parallax_js)) {
-//        //dpm('added js ' . $node_parallax_js);
-//        drupal_add_js($node_parallax_js);
-//    }
-    
-    
-    
+
+    drupal_add_js('sites/all/libraries/jquery.plugins/_sliders/slick/slick.js');
+    drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick/slick.css', array('weight' => 99, 'group' => 200));
+    drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick/slick-theme.css', array('weight' => 99, 'group' => 200));
+
+    drupal_add_js('sites/all/libraries/jquery.plugins/_sliders/slick-lightbox/dist/slick-lightbox.min.js');
+    drupal_add_css('sites/all/libraries/jquery.plugins/_sliders/slick-lightbox/dist/slick-lightbox.css', array('weight' => 99, 'group' => 200));
+
+    $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
+    drupal_add_js($path_to_custom_js . 'may_slick.js');
+    }
+   */
 }
 
-
+/**
+ *
+ * @param array $variables
+ */
 function bootstrap_may_process_html(&$variables) {
-//    dpm(drupal_get_title(), 'drupal_get_title() in bootstrap_may_process_html');
-//    //dpm($variables, '$variables in process_html');
-//    dpm($variables['head_title'], 'head_title in bootstrap_may_process_html');
-//    dpm($variables['head_title_array'], 'head_title_array in bootstrap_may_process_html');
-
-    // Remove Query Strings from CSS filenames (CacheBuster) 
-    //$variables['styles'] = preg_replace('/.css\?.*"/','.css"', $variables['styles']); 
-    $variables['styles'] = preg_replace('/style_custom.css\?.*"/','style_custom.css"', $variables['styles']); 
-    
+  // Remove Query Strings from CSS filenames (CacheBuster)
+  //$variables['styles'] = preg_replace('/.css\?.*"/','.css"', $variables['styles']);
+  $variables['styles'] = preg_replace('/style_custom.css\?.*"/', 'style_custom.css"', $variables['styles']);
 }
 
-/*
+/**
+ *
+ * @param type $variables
+ */
 function bootstrap_may_preprocess_html(&$variables) {
-//    dpm(drupal_get_title(), 'drupal_get_title() in bootstrap_may_preprocess_html');
-//    //dpm($variables, '$variables in process_html');
-//    dpm($variables['head_title'], 'head_title in bootstrap_may_preprocess_html');
-//    dpm($variables['head_title_array'], 'head_title_array in bootstrap_may_preprocess_html');
-
-    
+  //dpm($variables, '$variables in bootstrap_may_preprocess_html');
 }
-*/
