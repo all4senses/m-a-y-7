ca6caa962d95a7ed3a5544a293c53c93eda0acc1
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-11-13 05:17:05 +0700

    Multiupload

---
+++ modules/contrib/_Editor/ckeditor/plugins/extra_div_attr_fields/plugin.js
@@ -0,0 +1,206 @@
+/**
+ * @author all4senses
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+'use strict';
+
+( function() {
+    
+    // var img_attributes_to_exclude_from_adding = ['src', 'height', 'width', 'data-widget', 'sizes', 'srcset']; // 'sizes' and 'srcset' will be added by the imageresponsive plugin
+    // Allowed content from custom Drupal settings, set from a4s custom module via drupal_add_js()
+    if (typeof Drupal.settings.custom_div_attr_fields === 'undefined') {
+        Drupal.settings.custom_div_attr_fields = [];
+    }
+    if (typeof Drupal.settings.custom_attr_fields_textarea_type === 'undefined') {
+        Drupal.settings.custom_attr_fields_textarea_type = [];
+    }
+    
+    CKEDITOR.plugins.add( 'extra_div_attr_fields', {
+        //lang: 'en,de',
+        requires: 'widget,dialog', //'widget,dialog,image2',
+        init: function(editor) {
+            
+            CKEDITOR.on('dialogDefinition', function(e) {
+                
+                // Take the dialog name and its definition from the event data.
+                var dialogName = e.data.name;
+                var dialogDefinition = e.data.definition;
+                
+                // make sure this is the right editor (there can be more on one page) and the right dialog.
+                
+                //if ((e.editor != editor) || (dialogName != 'editdiv' && dialogName != 'editdiv_extra_1'))
+                if ((e.editor != editor) || (dialogName != 'editdiv' && dialogName.indexOf('editdiv_extra') == -1))
+                    return;
+             
+                 
+                // Create an extra tab    
+                var extra = dialogDefinition.getContents( 'extra' );
+                // Skip If it already exists
+                if (typeof extra !== 'undefined' && extra != null) {
+                    return;
+                }
+                
+                
+
+                // Copy the info tab and based on it create a new extra tab 
+                var infoTab = dialogDefinition.getContents( 'info' );
+                
+                if (typeof infoTab === 'undefined' || infoTab == null) {
+                    return;
+                }
+                
+                 
+                // Clone object of existing tab "info" to a new tab "extra"
+                extra = jQuery.extend(true, {}, infoTab);
+                
+                
+                
+                // And set new properties
+                extra.id = 'extra';
+                extra.label = 'Extra attributes';
+                extra.title = 'Extra attributes';
+                extra.elements = [];
+                // Add it to the dialog
+                dialogDefinition.addContents(extra,'info'); // Add a new tab before the Info tab
+
+                // Remove fields, that will be added to the Extra tab, from other tabs
+                var tabs_to_check = ['info', 'advanced'];
+                var checkTab = null;
+                var checkField = null;
+                jQuery.each( tabs_to_check, function( index1, value1 ){  
+                    checkTab = dialogDefinition.getContents( value1 );
+                    jQuery.each( Drupal.settings.custom_div_attr_fields, function( index2, value2 ){
+                        checkField = checkTab.get(value2);
+                        if (typeof checkField !== 'undefined' && checkField != null) {
+                            // We could add this field to a new tab right now, but we won't
+                            /*
+                            extra.add(checkField
+                                    //, 'class' // 'class' is the element id before which we want to add a new one
+                            );
+                            */
+                            checkTab.remove(value2);
+                        }
+                    });
+                });
+                
+                // Add extra attributes fields
+                jQuery.each( Drupal.settings.custom_div_attr_fields, function( index, value ){   
+                    // Skip diplicates 
+                    checkField = extra.get(value);
+                    if (typeof checkField === 'undefined' || checkField == null) {
+                        extra.add({ 
+                                id: value,
+                                type: (Drupal.settings.custom_attr_fields_textarea_type.indexOf(value) == -1) ? 'text' : 'textarea',
+                                //requiredContent: 'div[' + value + ']',
+                                label: value,
+                                // Works but we don't need it here
+                                /*
+                                setup: function(element) {
+                                            // So we chack for this
+                                            if (typeof element.$.attributes[value] === 'undefined' || element.$.attributes[value].value == 'undefined') {
+                                                this.setValue('');
+                                            }
+                                            else {
+                                                this.setValue(element.$.attributes[value].value);
+                                            }
+                                },
+                                commit: function( element ) {
+                                            element.setAttribute( value, this.getValue() );
+                                }
+                                */
+                            }//, 'class' // 'class' is the element id before which we want to add a new one
+                        );
+                    }
+                }); // jQuery.each( Drupal.settings.custom_div_attr_fields, function( index, value ){   
+                
+                
+                // Alter a bit a elementStyle field, so it wouldn't cause a problem with moved Style field (from advanced to extra tab)
+                
+                // All this functions below is to recreate all needed functions in new commit/onCange functions that are being altered
+                var elementStyleField = infoTab.get('elementStyle');
+                // Reuse the 'stylescombo' plugin's styles definition.
+                // Registered 'CKEDITOR.style' instances.
+		var styles = {};
+                editor.getStylesSet( function( stylesDefinitions ) {
+                        var styleName, style;
+
+                        if ( stylesDefinitions ) {
+                                // Digg only those styles that apply to 'div'.
+                                for ( var i = 0; i < stylesDefinitions.length; i++ ) {
+                                        var styleDefinition = stylesDefinitions[ i ];
+                                        //if ( styleDefinition.element && styleDefinition.element == 'div' ) 
+                                        {
+                                                styleName = styleDefinition.name;
+                                                styles[ styleName ] = style = new CKEDITOR.style( styleDefinition );
+
+//                                                if ( editor.filter.check( style ) ) {
+//                                                        // Populate the styles field options with style name.
+//                                                        stylesField.items.push( [ styleName, styleName ] );
+//                                                        stylesField.add( styleName, styleName );
+//                                                }
+                                        }
+                                }
+                        }
+
+//                        // We should disable the content element
+//                        // it if no options are available at all.
+//                        stylesField[ stylesField.items.length > 1 ? 'enable' : 'disable' ]();
+//
+//                        // Now setup the field value manually if dialog was opened on element. (#9689)
+//                        setTimeout( function() {
+//                                dialog._element && stylesField.setup( dialog._element );
+//                        }, 0 );
+                } );
+                           
+                // Synchronous field values to other impacted fields is required, e.g. div styles
+		// change should also alter inline-style text.
+		function commitInternally( targetFields ) {
+			var dialog = this.getDialog(),
+				element = dialog._element && dialog._element.clone() || new CKEDITOR.dom.element( 'div', editor.document );
+
+			// Commit this field and broadcast to target fields.
+			this.commit( element, true );
+
+			targetFields = [].concat( targetFields );
+			var length = targetFields.length,
+				field;
+			for ( var i = 0; i < length; i++ ) {
+				field = dialog.getContentElement.apply( dialog, targetFields[ i ].split( ':' ) );
+				field && field.setup && field.setup( element, true );
+			}
+		}
+                
+                elementStyleField.onChange = function() {
+                                    commitInternally.call( this, [ 'info:elementStyle', 'extra:class', 'advanced:dir', 'extra:style' ] );
+                                    };
+                elementStyleField.setup = function( element ) {
+                                                            if (!element && typeof Drupal.settings.cur_element_and_its_parents !== 'undefined' && typeof Drupal.settings.current_element_or_its_parent_index !== 'undefined') {
+                                                                element = Drupal.settings.cur_element_and_its_parents[Drupal.settings.current_element_or_its_parent_index];
+                                                            }
+                                                            for ( var name in styles ) {
+                                                                styles[ name ].checkElementRemovable( element, true, editor ) && this.setValue( name, 1 );
+                                                            }
+                                                        
+                                                        };
+                                                      
+                elementStyleField.commit = function( element ) {
+							var styleName;
+							if ( ( styleName = this.getValue() ) ) {
+								var style = styles[ styleName ];
+								if (typeof style !== 'undefined') {
+                                                                    style.applyToObject( element, editor );
+                                                                }
+							}
+							else {
+                                                                // a4s This caused empty style attribute after submitting
+								//element.removeAttribute( 'style' );
+							}
+                                                    };
+                
+                
+            }); // End of CKEDITOR.on('dialogDefinition', function(e) {
+        }
+    }); // End of CKEDITOR.plugins.add( 'extra_div_attr_fields', {
+    
+} )();
---
+++ modules/contrib/_Editor/ckeditor/plugins/extra_img_attr_fields/plugin.js
@@ -0,0 +1,163 @@
+/**
+ * @author all4senses
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+'use strict';
+
+( function() {
+    
+    var img_attributes_to_exclude_from_adding = ['src', 'height', 'width', 'data-widget', 'sizes', 'srcset']; // 'sizes' and 'srcset' will be added by the imageresponsive plugin
+    // Allowed content from custom Drupal settings, set from a4s custom module via drupal_add_js()
+    if (typeof Drupal.settings.custom_img_attr_fields === 'undefined') {
+        Drupal.settings.custom_img_attr_fields = [];
+    }
+    if (typeof Drupal.settings.custom_attr_fields_textarea_type === 'undefined') {
+        Drupal.settings.custom_attr_fields_textarea_type = [];
+    }
+    
+    CKEDITOR.plugins.add( 'extra_img_attr_fields', {
+        //lang: 'en,de',
+        requires: 'widget,dialog,image2',
+        beforeInit: function(editor) {
+            editor.on('widgetDefinition', function(e) {
+                var widget = e.data;
+                // figure out if this is the image dialog.
+                if(widget.name != 'image')
+                    return;
+
+                // should not happen but anyway...
+                if(!widget.allowedContent.img || !widget.allowedContent.img.attributes)
+                    return;
+                
+                // a4s
+                // Add to allowed content from custom Drupal settings, set from a4s custom module via drupal_add_js()
+                jQuery.each( Drupal.settings.custom_img_attr_fields, function( index, value ){
+                    if(widget.allowedContent.img.attributes.indexOf(value) == -1)
+                        widget.allowedContent.img.attributes += ',' + value;
+                });
+
+            });
+        },
+        init: function(editor) {
+            // Bind to widget#instanceCreated so we can see when the image widget is about to be initiated
+            editor.widgets.on('instanceCreated', function(e) {
+                var widget = e.data;
+
+                // figure out if this is the image dialog.
+                if(widget.name != 'image')
+                    return;
+
+                // register handler for data
+                widget.on('data', function(e) {
+
+                    widget = e.data;
+                    
+                    // keep extra attributes only when set.
+                    jQuery.each( Drupal.settings.custom_img_attr_fields, function( index, value ){
+                        if(widget[value])
+                            e.sender.parts.image.setAttribute(value, widget[value]);
+                        else
+                            e.sender.parts.image.removeAttribute(value); 
+                    });
+                });
+
+                // set data from existing variables.
+                var image = widget.element;
+                // since the img-tag can be wrapped with a caption, make sure we use the right element.
+                if(image.getName() != 'img')
+                    image = image.findOne('img');
+
+                //  a4s get all img attributes
+                var data = {};
+                // Add to allowed / newly added for editing content attributes names from a specific img tag (also added manually)
+                jQuery.each( image.$.attributes, function( index, value ){
+                    if(value.name.indexOf('data-cke') == -1 && img_attributes_to_exclude_from_adding.indexOf(value.name) == -1 && Drupal.settings.custom_img_attr_fields.indexOf(value.name) == -1) {
+                        Drupal.settings.custom_img_attr_fields.push(value.name);
+                    }
+                });
+               
+                jQuery.each( Drupal.settings.custom_img_attr_fields, function( index, value ){
+                    data[value] = image.getAttribute(value);
+                });
+                widget.setData(data);
+            });
+
+            CKEDITOR.on('dialogDefinition', function(e) {
+                // make sure this is the right editor (there can be more on one page) and the right dialog.
+                if ((e.editor != editor) || (e.data.name != 'image2'))
+                    return;
+
+                var dialogDefinition = e.data.definition;
+                
+                // Create an extra tab    
+                var extra = dialogDefinition.getContents( 'extra' );
+                // Skip If it already exists
+                if (typeof extra !== 'undefined' && extra != null) {
+                    return;
+                }
+
+                // Copy the info tab and based on it create a new extra tab 
+                var infoTab = dialogDefinition.getContents( 'info' );
+                if (typeof infoTab === 'undefined' || infoTab == null) {
+                    return;
+                }
+                infoTab.label = 'Image basics'
+                
+                // Clone object of existing tab "info" to a new tab "extra"
+                extra = jQuery.extend(true, {}, infoTab);
+                // And set new properties
+                extra.id = 'extra';
+                extra.label = 'Extra attributes';
+                extra.title = 'Extra attributes';
+                extra.elements = [];
+                // Add it to the dialog
+                dialogDefinition.addContents(extra,'info'); // Add a new tab before the Info tab
+
+                // Remove fields, that will be added to the Extra tab, from other tabs
+                var tabs_to_check = ['info'];
+                var checkTab = null;
+                var checkField = null;
+                jQuery.each( tabs_to_check, function( index1, value1 ){  
+                    checkTab = dialogDefinition.getContents( value1 );
+                    jQuery.each( Drupal.settings.custom_img_attr_fields, function( index2, value2 ){
+                        checkField = checkTab.get(value2);
+                        if (typeof checkField !== 'undefined' && checkField != null) {
+                            // We could add this field to a new tab right now, but we won't
+                            /*
+                            extra.add(checkField
+                                    //, 'class' // 'class' is the element id before which we want to add a new one
+                            );
+                            */
+                            checkTab.remove(value2);
+                        }
+                    });
+                });
+                
+                Drupal.settings.custom_attr_fields_textarea_type
+                
+                // Add extra attributes fields
+                jQuery.each( Drupal.settings.custom_img_attr_fields, function( index, value ){   
+                    // Skip duplicates 
+                    checkField = extra.get(value);
+                    if (typeof checkField === 'undefined' || checkField == null) {
+                        extra.add({
+                            id: value,
+                            type: (Drupal.settings.custom_attr_fields_textarea_type.indexOf(value) == -1) ? 'text' : 'textarea',
+                            requiredContent: 'img[' + value + ']',
+                            label: value,
+                            setup: function(widget) {
+                                this.setValue(widget.data[value]);
+                            },
+                            commit: function (widget) {
+                                widget.setData(value, this.getValue());
+                            }
+                        });
+                    }
+                }); // jQuery.each( Drupal.settings.custom_img_attr_fields, function( index, value ){   
+                
+                
+            });
+        }
+    });
+} )();
