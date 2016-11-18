92c14a189c7026b2e3de6c9bfca2c17ad20b5e8e
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-11-19 04:29:01 +0700

    Infinite scroll

--- modules/contrib/_Views/views_infinite_scroll/views-infinite-scroll.js
+++ modules/contrib/_Views/views_infinite_scroll/views-infinite-scroll.js
@@ -15,6 +15,7 @@
   $.fn.infiniteScrollInsertView = function ($new_view) {
     var $existing_view = this;
     var $existing_content = $existing_view.find('.view-content').children();
+    console.log($existing_content, '$existing_content');
     $new_view.find('.view-content').prepend($existing_content);
     $existing_view.replaceWith($new_view);
     $(document).trigger('infiniteScrollComplete', [$new_view, $existing_content]);
