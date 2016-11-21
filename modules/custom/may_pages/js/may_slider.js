b24e69a7bd74ed1877ac810720b2e52789bf1dce
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-11-22 02:41:10 +0700

    New slider

---
+++ modules/custom/may_pages/js/may_slider.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+    Drupal.behaviors.may_slider = {
+        attach: function (context, settings) {
+
+            console.log('may_slider.....');
+
+          
+          
+        } // End of Attach
+    }; // End of Behavior
+
+}(jQuery));
+
