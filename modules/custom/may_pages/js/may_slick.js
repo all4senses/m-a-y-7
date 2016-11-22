49d605b8e8e7d50daba1345a206962d7fbba83f5
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-11-23 04:43:35 +0700

    Slick fixes

--- modules/custom/may_pages/js/may_slick.js
+++ modules/custom/may_pages/js/may_slick.js
@@ -68,7 +68,8 @@
                     };
                     
                     console.log('$(this): ', $(this));
-                    $(this)[0].slick('slickSetOption', 'responsive', [{ breakpoint: 768, settings: {slidesToShow: visible_num, } }], true);
+                    jQuery('.slides.p2-12.s1').slick('slickSetOption', 'responsive', [{ breakpoint: 768, settings: {slidesToShow: 2, vertical: true} }], true);
+                    //$(this)[0].slick('slickSetOption', 'responsive', [{ breakpoint: 768, settings: {slidesToShow: visible_num, } }], true);
                     console.log('+++> Slick: ', slick);
 
                 })
