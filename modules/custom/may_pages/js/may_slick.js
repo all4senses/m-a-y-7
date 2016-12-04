ab34a6a3988eab999bfb421e8fe4e30d7a9fdad3
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-12-04 19:00:39 +0700

    Project 1-2

--- modules/custom/may_pages/js/may_slick.js
+++ modules/custom/may_pages/js/may_slick.js
@@ -11,21 +11,22 @@
 })(jQuery);
 */
 
+function closest (num, arr) {
+    var curr = arr[0];
+    var diff = Math.abs (num - curr);
+    for (var val = 0; val < arr.length; val++) {
+        var newdiff = Math.abs (num - arr[val]);
+        if (newdiff < diff) {
+            diff = newdiff;
+            curr = arr[val];
+        }
+    }
+    return curr;
+}
 
 (function ($) {
     
-    function closest (num, arr) {
-        var curr = arr[0];
-        var diff = Math.abs (num - curr);
-        for (var val = 0; val < arr.length; val++) {
-            var newdiff = Math.abs (num - arr[val]);
-            if (newdiff < diff) {
-                diff = newdiff;
-                curr = arr[val];
-            }
-        }
-        return curr;
-    }
+    
             
 
     Drupal.behaviors.may_slick = {
