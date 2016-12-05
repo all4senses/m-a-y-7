017786a4f8e9d896e69c80c5f6ca9aeee50c2caa
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-12-05 20:42:03 +0700

    Pictures by type

--- modules/custom/may_pages/templates/images-by-type.tpl.php
+++ modules/custom/may_pages/templates/images-by-type.tpl.php
@@ -62,7 +62,8 @@
             $out = '';
             foreach($images as $image) {
               //$out .= '<div class="item"><img class="item-img" style="float:left;" width="300" src="' . str_replace('public://', '/f/', $image->uri) . '"/></div>';
-              $out .= '<div class="masonry-item"><img class="item-img" style="float:left;" src="' . str_replace('public://', '/f/', $image->uri) . '"/></div>';
+              //$out .= '<div class="masonry-item"><img class="masonry-item-img" style="float:left;" src="' . str_replace('public://', '/f/', $image->uri) . '"/></div>';
+              $out .= '<div class="masonry-item"><img class="masonry-item-img" src="' . str_replace('public://', '/f/', $image->uri) . '"/></div>';
             }
             $out = '<div class="masonry-items">' . $out . '</div>';
           }
