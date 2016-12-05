9b416b8b36d9e36f7d090ab8d8b0385b2c2e970f
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-12-05 23:22:53 +0700

    Pictures by type

--- modules/custom/may_pages/js/may_masonry.js
+++ modules/custom/may_pages/js/may_masonry.js
@@ -24,6 +24,13 @@
             $grid.imagesLoaded().progress( function() {
                 $grid.masonry('layout');
             });
+            
+            
+            new AnimOnScroll( document.getElementById( 'grid' ), {
+                    minDuration : 0.4,
+                    maxDuration : 0.7,
+                    viewportFactor : 0.2
+            } );
 
 
           
