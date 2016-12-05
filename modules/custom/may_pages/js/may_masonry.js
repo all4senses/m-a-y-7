dc924919e5665b7e63ba92b8610b9ed9a427a0aa
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-12-05 20:48:53 +0700

    Pictures by type

--- modules/custom/may_pages/js/may_masonry.js
+++ modules/custom/may_pages/js/may_masonry.js
@@ -4,11 +4,11 @@
         attach: function (context, settings) {
 
             console.log('may_masonry.....');
-            jQuery('.masonry-items').masonry({
-                // options...
-                itemSelector: '.masonry-item',
-                columnWidth: 200
-            });
+//            jQuery('.masonry-items').masonry({
+//                // options...
+//                itemSelector: '.masonry-item',
+//                columnWidth: 200
+//            });
           
         } // End of Attach
     }; // End of Behavior
